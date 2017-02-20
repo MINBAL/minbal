0aaf6bad62d0dd5d7fda19dd75bf69df46d29199
jmelvnsn <jlevinson@theonion.com>
2017-02-20 13:09:31 -0600

    Setting up new framework

--- config.rb
+++ config.rb
@@ -9,6 +9,8 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+page "/studio/*", :layout => "layout"
+
 # With alternative layout
 # page "/path/to/file.html", layout: :otherlayout
 
