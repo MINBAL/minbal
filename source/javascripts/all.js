0ba047e19bd97e99bcafcdebdc7e4229d4ae37f8
jmelvnsn <jlevinson@theonion.com>
2017-02-25 13:59:19 -0600

    Setting up page push unit

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,16 @@
 // This is where it all goes :)
+
+var pushContainer = document.querySelector('main');
+var toggleButton = document.querySelector('.hamburger');
+
+function deployMenu() {
+
+  if(pushContainer.classList.contains('open')) {
+    pushContainer.classList.remove('open');
+  }
+  else if(!pushContainer.classList.contains('open')) {
+    pushContainer.classList.add('open');
+  }
+}
+
+toggleButton.addEventListener('click', deployMenu);
