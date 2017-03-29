4a191bcdaa2358483771d2e24b1d9e76e9d9ac4f
jmelvnsn <jlevinson@theonion.com>
2017-03-29 00:18:23 -0500

    Add composerly to licensing view

--- config.rb
+++ config.rb
@@ -12,7 +12,7 @@ page '/*.json', layout: false
 page '/*.txt', layout: false
 
 page "/studio/*", :layout => "layout"
-page "/licensing/*", :layout => "layout"
+page "/licensing/*", :layout => "licensing-layout"
 page "/composition/*", :layout => "layout"
 
 # With alternative layout
