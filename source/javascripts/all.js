987a7029c16910b750bd96386203105f21abf89a
jmelvnsn <jlevinson@theonion.com>
2018-03-19 11:09:43 -0500

    Video player

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,6 +3,8 @@ var myLazyLoad = new LazyLoad();
 var pushContainer = document.querySelector('main');
 var toggleMenuButton = document.querySelector('.toggle-menu-btn');
 var pagePushNav = document.querySelector('.page-push-nav');
+var videoPosters = document.getElementsByClassName('video-poster');
+var videoPlayer = document.getElementById('video-player');
 
 function toggleMenu() {
   if(pushContainer.classList.contains('open')) {
@@ -11,6 +13,15 @@ function toggleMenu() {
   else if(!pushContainer.classList.contains('open')) {
     pushContainer.classList.add('open');
   }
+}
+
+function toggleVideoPlayer() {
+	videoPlayer.setAttribute('src', this.dataset.embed);
+	videoPlayer.scrollIntoView({ behavior: 'smooth' });
+}
+
+for (var i = 0; i < videoPosters.length; i++) {
+  videoPosters[i].addEventListener('click', toggleVideoPlayer, false);
 }
 
 toggleMenuButton.addEventListener('click', toggleMenu);
